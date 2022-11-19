e4359fc80e7abf4d974d9d1dbf8c61ef8a45f46c
Kyle Kent <kylekent@Jodis-iPad.localdomain>
2022-11-19 15:05:40 -0600

    able to display image

---
+++ image_to_text/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
